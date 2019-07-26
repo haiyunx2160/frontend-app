055a30b7c0749a23b7e2dbdbdc6f865ec2a6299a
Aaron <haiyunx2160@gmail.com>
2019-07-26 01:56:36 -0400

    add sign in page validation

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -3,24 +3,46 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {fetchToken} from '../../actions/signInActions';
 import {NavLink, Redirect} from 'react-router-dom';
+import {Field, reduxForm} from "redux-form";
+
 
 import './SignIn.scss';
 import {SIGN_IN} from "../../actions/types";
 
 class SignIn extends Component {
 
-    onSignUpSubmit = (e) => {
-        e.preventDefault();
-        e.persist();
+    onSignUpSubmit = (formValues) => {
+
         this.props.dispatch({
             type: SIGN_IN,
             payload: {
-                username: e.target[0].value,
-                password: e.target[1].value,
+                username: formValues.username,
+                password: formValues.password
             }
         })
     };
+renderError=({error, touched})=>{
+if(touched && error){
+    return (
+        <small style={{color:'red'}}>{error}</small>
+    )
+}
+};
 
+    renderInput = (formProps) => {
+        console.log(formProps.meta)
+        return (
+
+            <div className='form-group'>
+                <label htmlFor="">{formProps.label}</label>
+                <input  {...formProps.input}
+                        className='form-control'
+                        autoComplete='off'
+                        type={formProps.type}/>
+                {this.renderError(formProps.meta)}
+            </div>
+        )
+    };
 
     render() {
 
@@ -28,18 +50,20 @@ class SignIn extends Component {
         return (
             <div className="signin-container">
                 <div>
-                    <form onSubmit={(e) => {
-                        this.onSignUpSubmit(e)
-                    }}>
-                        <div className="form-group">
-                            <input type="text" className="form-control" id="userName" placeholder="Username"
-                                   required />
-                        </div>
-                        <div className="form-group">
-                            <input type="password" className="form-control" id="password" placeholder="Password"
-                                   required />
-                        </div>
-                        <small style={{display:this.props.error?'inline':'none', color:'red'}}>Wrong user name or password, please try again</small>
+                    <form onSubmit={this.props.handleSubmit(this.onSignUpSubmit)}>
+                        <Field name='username'
+                               type='text'
+                               label='UserName'
+                               component={this.renderInput}/>
+
+                        <Field name='password'
+                               label='Password'
+                               type='password'
+                               component={this.renderInput}/>
+
+                        <small style={{display: this.props.error ? 'inline' : 'none', color: 'red'}}>Wrong user name or
+                            password, please try again
+                        </small>
                         <div>
                             <button type="submit" name="signIn" className="btn btn-info signin-btn">Login</button>
                         </div>
@@ -54,11 +78,23 @@ class SignIn extends Component {
     }
 }
 
+const validate = (formValues)=>{
+    const errs ={};
+    if (!formValues.username){
+        errs.username='Username can not be empty'
+    }
+    if (!formValues.password){
+        errs.password='Password can not be empty'
+    }
+
+    return errs;
+};
+
 function mapStateToProps(state) {
     console.log(state);
     return {
         authenticated: state.UserStore.authenticated,
-        error:state.UserStore.error
+        error: state.UserStore.error
     }
 }
 
@@ -73,7 +109,11 @@ function mapDispatchToProps(dispatch) {
     }
 }
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(SignIn)
+
+const signinForm = connect(mapStateToProps, mapDispatchToProps)(SignIn);
+
+export default reduxForm({
+    form: 'signin_form',
+    validate
+
+})(signinForm)
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { default as UserStore } from './userReducer';
+import {reducer} from "redux-form";
 
 const appReducer = combineReducers({
-  UserStore
+  UserStore:UserStore,
+  form:reducer
 });
 
 const rootReducer = (state, action) => {
